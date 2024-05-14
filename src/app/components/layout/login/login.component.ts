33889dff8365628826bde0bec4e1d882ed9cea9c
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-05-14 00:56:56 -0300

    feat: add components/layout/login

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  usuario!: string;
+  senha!: string;
 
+  router = inject(Router);
+
+  logar() {
+    if (this.usuario != 'admin') {
+      Swal.fire({
+        title: 'Usuario não existente!',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+    } else if (this.senha != 'admin') {
+      Swal.fire({
+        title: 'Senha equivocada!',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+    } else {
+      this.router.navigate(['admin/autores'])
+    }
+  }
 }
